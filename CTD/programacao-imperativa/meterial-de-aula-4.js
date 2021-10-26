ef6a71063dc305e79cb3d597f27cc86b9c3e9e06
Vjoaoa <89075716+Vjoaoa@users.noreply.github.com>
2021-10-26 19:29:37 -0300

    Add files via upload

---
+++ CTD/programacao-imperativa/meterial-de-aula-4.js
@@ -0,0 +1,9 @@
+let nome = ("José");
+let idade = (27);
+let peso = (83.5);
+let altura = (1.70);
+let plano = "ouro"
+let imc = peso / (altura * altura);
+console.log (imc);
+console.log (nome+ " tem " +idade+ " anos e seu índice de massa corporal é de" +imc+".")
+
