ee07280e17dee7da4ea95a0e0714aeda363380d2
vjoao <jl159456@gmail.com>
2021-10-29 15:20:23 -0300

    teste

---
+++ programacao-imperativa/Aula5.js
@@ -0,0 +1,56 @@
+function soma (x,y){//criar função
+    return x + y;//retorna 
+    
+}
+console.log (soma (15 , 5)); // printei retorno da função
+
+function soma (nome,idade){//criar função
+    return nome+" "+idade;//retorna calculo
+}
+console.log (soma ("João", 19)); // printei retorno da função'
+
+function somar(x,y){
+    return x+y;
+}
+let result = somar (15,5)
+console.log(somar(30,10));
+console.log(somar(100,1));
+console.log(result);
+console.log(soma(0,1));
+
+//material de aula 05
+//exercio 01
+function polegadas(x,y){
+    return x/y;
+}
+console.log (polegadas(10,2.54));
+//exercio 02
+function link() {
+    console.log("http://www.funcaojs.com.br");
+   }
+   
+   link()
+      
+//exercio 03
+function mesmafra (nome){//criar função
+    return nome+" ! ";//retorna calculo
+}
+console.log (mesmafra ("mesma frase"));
+
+//exercio 04
+function cachorro(x,y){
+    return x*y;
+}
+console.log (cachorro(2,7));
+
+//exercio 06
+
+
+let peso = (83.5);
+let altura = (1.70);
+let imc = peso / (altura * altura);
+console.log (imc);
+console.log ( "seu índice de massa corporal é de" +imc+".");
+
+
+
---
+++ programacao-imperativa/aula6.js
@@ -0,0 +1,48 @@
+function soma (x,y){
+    return x + y;
+}
+console.log ("-------------- Teste de Operações / Calculadora --------------")
+console.log (soma (5,5)); 
+
+function subtracao (x,y){
+    return x - y;
+}
+console.log (subtracao(5,5))
+console.log ("-------------- Teste de Operações / Calculadora --------------")
+
+
+function mutiplicacao (x,y){
+    return x * y;
+}
+console.log (mutiplicacao(5,5))
+console.log ("-------------- Teste de Operações / Calculadora --------------")
+
+function divisao (x,y){
+    return x / y;
+}
+console.log (divisao(5,5))
+console.log ("-------------- Teste de Operações / Calculadora --------------")
+
+
+function quadraDoNumero(x,y){
+    return mutiplicacao(x,y)
+}
+console.log (quadraDoNumero(3,3))
+console.log ("-------------- Teste de Operações / Calculadora --------------")
+
+
+function mediaDeTresNumeros(x,y,z){
+    let soma2 = adicionar (a,b);
+    let soma3 = adicionar (soma2,c);
+    return divisao (soma3,3);
+}
+console.log ("-------------- Teste de Operações / Calculadora --------------")
+
+
+function calculaPorcentagem(x,y){
+    return mutiplicacao(x , y/ 100 ) 
+    
+    
+}
+console.log (calculaPorcentagem(300,15))
+console.log ("-------------- Teste de Operações / Calculadora --------------")
