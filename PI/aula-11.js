b6ad65192a52929ad34acac1d9a5c6ba806a9060
vjoao <jl159456@gmail.com>
2021-11-16 23:20:41 -0300

    aula16/11/2021

---
+++ PI/aula-11.js
@@ -0,0 +1,27 @@
+// for(let i=0; i<=1; i++){
+//     console.log("olá mundo")
+}
+let peliculas = ["star wars", "totoro",  "rocky", "pulp fiction",  "la vida es bella"]
+
+for(let i=0; i<peliculas.leght; i++){
+    function convertirAMayusculas(array){
+        array[0] = array[0].toUpperCase()
+        array[1] = array[1].toUpperCase()
+        array[2] = array[2].toUpperCase()
+        array[3] = array[3].toUpperCase()
+        array[4] = array[4].toUpperCase()
+        return array
+}
+
+}
+console.log(convertirAMayusculas(peliculas)
+ 
+function pasajeDeElementos(array1, array2) { 
+    array1.push(array2.pop().  toUpperCase()) 
+    array1.push(array2.pop(). toUpperCase()) 
+    array1.push(array2.pop(). toUpperCase()) 
+    array1.push(array2.pop(). toUpperCase()) 
+    array1.push(array2.pop(). toUpperCase()) 
+    return array1,array2 
+   }
+   
