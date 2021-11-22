4aedc6e67b611d59a4228dca0e3ecfc76ecd5481
vjoao <jl159456@gmail.com>
2021-11-22 19:13:02 -0300

    aulas-novas

--- PI/aula-11.js
+++ PI/aula-11.js
@@ -1,27 +1,17 @@
 // for(let i=0; i<=1; i++){
 //     console.log("olá mundo")
-}
-let peliculas = ["star wars", "totoro",  "rocky", "pulp fiction",  "la vida es bella"]
+let filmes= ["star wars", "totoro",  "rocky", "pulp fiction",  "a vida é bela"]
 
-for(let i=0; i<peliculas.leght; i++){
-    function convertirAMayusculas(array){
-        array[0] = array[0].toUpperCase()
-        array[1] = array[1].toUpperCase()
-        array[2] = array[2].toUpperCase()
-        array[3] = array[3].toUpperCase()
-        array[4] = array[4].toUpperCase()
-        return array
-}
-
-}
-console.log(convertirAMayusculas(peliculas)
- 
-function pasajeDeElementos(array1, array2) { 
-    array1.push(array2.pop().  toUpperCase()) 
-    array1.push(array2.pop(). toUpperCase()) 
-    array1.push(array2.pop(). toUpperCase()) 
-    array1.push(array2.pop(). toUpperCase()) 
-    array1.push(array2.pop(). toUpperCase()) 
-    return array1,array2 
-   }
-   
+function converterMaiusculas(array){
+    for (let i = 0; i < array.length; i++) {
+        // const element = array[i];
+        
+    }
+      array[0] = array[0].toUpperCase()
+      array[1] = array[1].toUpperCase()
+      array[2] = array[2].toUpperCase()
+      array[3] = array[3].toUpperCase()
+      array[4] = array[4].toUpperCase()
+      return array
+    }
+    console.log (converterMaiusculas(array.length));
---
+++ PI/aula-12.js
@@ -0,0 +1,54 @@
+const alicia = [23, 69, 32];
+ const bob = [12, 67, 43];
+
+ function Ganhador(a, b) {
+    let aa=0
+    let bb=0
+    for (let i = 0; i <a.length; i++) {
+        if (a[i]>b[i]) {
+            aa++;
+        }
+        else if (b[i]> a[i]) {
+            bb++;}
+    }
+    if (aa>bb) {
+        return "Parcipante a"
+    }
+    else if (aa<bb) {
+        return "Participante b"
+    }
+    else{
+        return "Empate"
+    }
+}
+
+console.log ("ganhador é: " + Ganhador(alicia, bob));
+
+function digitalHouse(x,y) {
+    for (let i = 0; i <= 100; i++) {
+        if (i % x ==0 && i % y ==0) {
+            console.log("Digital House")
+        }
+        else{
+            if (i % x == 0) {
+                console.log("Digital")
+            }
+            else if (i % y ==0) {
+                console.log ("House")
+            }
+            else{
+                console.log(i)
+            }
+        }
+    }
+}
+
+console.log(digitalHouse(2,9));
+
+const elements = ["t","c","h","a","u"];
+
+console.log(elements.join(''));
+
+const ola = ["o","l","á"];
+
+console.log(ola.join(''));
