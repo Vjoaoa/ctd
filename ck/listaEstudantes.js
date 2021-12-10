4bd07b8f4314102e34654ec0db263b78c6974dce
vjoao <jl159456@gmail.com>
2021-12-09 23:01:57 -0300

    a

---
+++ ck/listaEstudantes.js
@@ -0,0 +1,11 @@
+listaEstudantes = [
+    { nome: "paulo", qtdFaltas: 1, notas: [10, 10, 10] },
+    { nome: "rogerio", qtdFaltas: 3, notas: [10, 8, 9] },
+    { nome: "juliana", qtdFaltas: 2, notas: [8.7, 7, 8.8] },
+    { nome: "mateus", qtdFaltas: 3, notas: [8.7, 6, 9] },
+    { nome: "joão", qtdFaltas: 2, notas: [7, 8.8, 5] },
+    
+  ];
+  
+  
+  module.exports = JSON.stringify(listaEstudantes);
